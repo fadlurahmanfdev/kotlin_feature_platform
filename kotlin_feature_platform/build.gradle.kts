d91064301b8a8186a10444358c4b50b936a6c459
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-12-16 13:25:23 +0700

    update platform

--- kotlin_feature_platform/build.gradle.kts
+++ kotlin_feature_platform/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
+
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -53,6 +57,39 @@ publishing {
             afterEvaluate {
                 from(components["release"])
             }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("com.fadlurahmanfdev", "feature_platform", "0.0.1")
+
+    pom {
+        name.set("Feature Platform")
+        description.set("Library to simplified platform implementation")
+        inceptionYear.set("2024")
+        url.set("https://github.com/fadlurahmanfdev/kotlin_feature_platform/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("fadlurahmanfdev")
+                name.set("Taufik Fadlurahman Fajari")
+                url.set("https://github.com/fadlurahmanfdev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/fadlurahmanfdev/kotlin_feature_platform/")
+            connection.set("scm:git:git://github.com/fadlurahmanfdev/kotlin_feature_platform.git")
+            developerConnection.set("scm:git:ssh://git@github.com/fadlurahmanfdev/kotlin_feature_platform.git")
         }
     }
 }
